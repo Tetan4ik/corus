c05aa6b6d9dee880d04ee9df4147eb6e6a41995f
vadim <v.tarkhov@runetsoft.ru>
2019-08-29 10:33:02 +0300

    Старт

---
+++ src/User.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @Entity @Table(name="users")
+ */
+Class User
+{
+
+    /** @id @Column(type="integer") @GeneratedValue */
+    protected $id;
+    /** @Column(type="string")  */
+    protected $name;
+    /** @Column(type="string")  */
+    protected $password;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+
+
+}
+
+
+?>
